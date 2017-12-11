6152bc95a655458cf1dc353d17a09d5e6be7f49b
王朋 <294336370@qq.com>
2017-12-11 12:20:54 +0800

    0-0

--- pages/banner/banner.js
+++ pages/banner/banner.js
@@ -4,13 +4,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+  var post_content1={
+    date:"sep 18 2017",
+    title:"正是虾肥蟹壮时",
+    content:"还时常记得出游溪亭，一玩就玩到日黑天暮，深深地沉醉，而忘记归路。一直玩到兴尽，回舟返途，却迷途进入藕花的深处。大家争着划呀，船儿抢着渡，惊起了满滩的鸥鹭。",
+    view_num:"121",
+    collect_num:"96",
+    image:{
+      author_img: "/images/1.png",
+      post_img: "/images/1.png",
+    }
+  }
+  this.setData(post_content1);
     
   },
 
